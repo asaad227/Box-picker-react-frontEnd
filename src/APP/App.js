f974a59ca65a43cf2000646b7ba6729bbe4e05f4
asaad227 <a.saad227@yahoo.com>
2023-03-21 17:37:46 +0000

    function name update and css update

--- src/APP/App.js
+++ src/APP/App.js
@@ -5,20 +5,18 @@ import './App.css';
 
 function App() {
   const [score, setScore] = useState(0);
-   
-    
-  function btn1(){
+  
+  function handleClick(){
       const randomM = Math.floor(Math.random() * 2);
       const randomT = Math.floor(Math.random() * 2);
       if(randomM === randomT){
-          console.log("I have clicked 1st")
           setScore(score + 10)
       }
   }
   return (
     <div className="App">
-  <Fetch />
-  <Button btn1={btn1} score={score} />
+  {/* <Fetch /> */}
+  <Button  handleClick={handleClick} score={score} />
 
      
     </div>
