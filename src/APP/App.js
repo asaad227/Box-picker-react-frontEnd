5f9e48dfaef0d7718a01b85eb8f2ee915d00dd11
asaad227 <a.saad227@yahoo.com>
2023-03-21 23:59:03 +0000

    game home work in progress and some css update

--- src/APP/App.js
+++ src/APP/App.js
@@ -4,19 +4,17 @@ import Fetch from '../Fetch';
 import './App.css';
 
 function App() {
-  const [score, setScore] = useState(0);
-  
-  function handleClick(){
-      const randomM = Math.floor(Math.random() * 2);
-      const randomT = Math.floor(Math.random() * 2);
-      if(randomM === randomT){
-          setScore(score + 10)
-      }
-  }
+
+
+
+
+
+
+
   return (
     <div className="App">
   {/* <Fetch /> */}
-  <Button  handleClick={handleClick} score={score} />
+  <Button />
 
      
     </div>
